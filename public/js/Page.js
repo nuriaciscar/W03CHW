f55506769c68c34b67a80ec1ff2ac4245e23f126
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-16 19:49:33 +0200

    Created class Page with constructor, html and pokemonCard

--- public/js/Page.js
+++ public/js/Page.js
@@ -12,14 +12,53 @@ class Page extends Component {
     this.parentElement = parentElement;
     this.element = document.createElement(htmlTag);
     this.element.className = className;
-  }
 
-  (async() {
+    this.generateHTML();
+  
+
+  ( async() {
     const getPokemon = new PokemonService(this.urlPokemon);
     const showPokemon = await getPokemon.getPokemons(this.urlPokemon);
     this.pokemonList= showPokemon.results;
 
-    this.pokemonList.map((character) => new PokemonCard ()
+    this.pokemonList.map((character) => new PokemonCard (containerPokemons, "pokemon", "li", character.url));
+
+    })();
   }
-  )
+
+  generateHTML(){
+    this.element.innerHTML=
+    `
+    <header class="header">
+      <h1>
+        <img
+          class="logo"
+          src="./img/logo.png"
+          alt="Pokemons"
+          width="120px"
+          alt="120px"
+        />
+      </h1>
+      <ul>
+        <li>Mis pokemons</li>
+        <li>Detalles</li>
+      </ul>
+    </header>
+    <main>
+      <h2>All</h2>
+      <section class="all">
+        <div class="all__cards">
+          <ul class="pokemons-list">
+          </ul>
+        </div>
+      </section>
+    </main>
+    <footer class="footer"></footer>`;
+    
+    const containerPokemons = document.querySelector(".pokemons-list");
+
+  }
 }
+
+
+export default Page;
